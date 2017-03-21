0634dcecd03308888184fa94e7a0a3fb3ff48b45
Kris Sparks <kbs5280@gmail.com>
2017-03-21 11:53:24 -0600

    Add mouse enters and mouse leaves
    Currently logs string to console
    Need to add an Axios call to render songs

--- src/artist.js
+++ src/artist.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class Artist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovered: false
+    }
+  }
+
+  mouseEnter() {
+    console.log('mouse enters');
+    this.setState({ hovered: true });
+  }
+
+  mouseLeave() {
+    console.log('mouse leaves');
+    this.setState({ hovered: false });
+  }
+
   render() {
     return (
-      <div className='artist'>
+      <div className='artist'
+           onMouseEnter={this.mouseEnter.bind(this)}
+           onMouseLeave={this.mouseLeave.bind(this)}>
         {this.props.name}
       </div>
     )
