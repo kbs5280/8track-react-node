edd699d7fb2927001d460b362fbd369772bbf250
Kris Sparks <kbs5280@gmail.com>
2017-03-20 18:03:32 -0600

    Axios call to external API returns artists data
    Currently logging to console.
    Need to pass props to create-artist.js
    Need to render artists to DOM.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import './App.css';
 import Header from './header';
 import CreateArtist from './create-artist';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artists: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get('https://node-8track.herokuapp.com/api/v1/artists')
+      .then((response) => {
+        console.log(response.data);
+        // this.setState({ artists: response.data });
+      });
+  }
+
   render() {
     return (
       <div>
